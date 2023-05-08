66a25f239fe5444a79045ea37db42091ef806de0
Luke Lai <lukelai.dev@gmail.com>
2023-05-08 15:03:44 +0100

    Initial Commit

---
+++ InductionApp/InductionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InductionAppApp.swift
+//  InductionApp
+//
+//  Created by Luke Lai on 08/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct InductionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
