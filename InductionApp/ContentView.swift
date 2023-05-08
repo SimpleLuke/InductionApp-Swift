dcd89ad3bade68facfa44e430a5d7fca409b4f3b
Luke Lai <lukelai.dev@gmail.com>
2023-05-08 17:44:06 +0100

    Add basic layout of login view

--- InductionApp/ContentView.swift
+++ InductionApp/ContentView.swift
@@ -7,17 +7,60 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LoginView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("This is the login view")
     }
 }
+
+struct ContentView: View {
+    @State private var email = ""
+    @State private var password = ""
+    @State private var wrongEmail = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color.blue
+                    .ignoresSafeArea()
+                Circle().scale(1.7).foregroundColor(.white.opacity(0.15))
+                Circle().scale(1.35).foregroundColor(.white)
+                
+                VStack {
+                    Text("Login")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    TextField("Email",text:$email)
+                        .padding()
+                        .frame(width: 300,height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red,width: CGFloat(wrongEmail))
+                    SecureField("Password",text:$password)
+                        .padding()
+                        .frame(width: 300,height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red,width: CGFloat(wrongPassword))
+                    Button("Login"){
+                        showingLoginScreen.toggle()
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 300,height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                }
+            }
+            .navigationDestination(isPresented: $showingLoginScreen) {
+                LoginView()
+              }
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
