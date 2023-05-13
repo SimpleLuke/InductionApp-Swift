75c851d7183aa8c110cff1332098cd37b72ef2a7
Luke Lai <lukelai.dev@gmail.com>
2023-05-13 19:50:16 +0100

    Remove commented code

--- InductionApp/Controllers/ChapterController.swift
+++ InductionApp/Controllers/ChapterController.swift
@@ -9,17 +9,6 @@ import Foundation
 import SwiftUI
 
 class ChapterController {
-//    func selectChapter(name:String,user:User) -> some View {
-//        switch name {
-//            case "HL 5 Key Values":
-//            return AnyView(FiveKeyValuesView(user:$user))
-//            case "The HL Way":
-//               return AnyView(HLWayView())
-//            default:
-//               return AnyView(ChapterNotFoundView())
-//        }
-//    }
-    
     func getCompletedChapters(id:Int, completion: @escaping (CompletedChapter?, Error?) -> Void) {
         guard let url = URL(string: "http://localhost:5000/chapters/completed/\(id)") else {
             print("Invalid URL")
